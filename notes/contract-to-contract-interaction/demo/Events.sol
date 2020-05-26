8d8ce582ed5c261aa8e744a54aeef972475ca04b
dhruvinparikh93 <imsilent93@gmail.com>
2020-05-26 19:00:05 -0400

    topic demo

---
+++ notes/contract-to-contract-interaction/demo/Events.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+contract ClientReceipt {
+    event Deposit(
+        address indexed _from,
+        bytes4 indexed _id,
+        uint _value
+    );
+
+    function deposit(bytes4 _id) public payable {
+        // Events are emitted using `emit`, followed by
+        // the name of the event and the arguments
+        // (if any) in parentheses. Any such invocation
+        // (even deeply nested) can be detected from
+        // the JavaScript API by filtering for `Deposit`.
+        emit Deposit(msg.sender, _id, msg.value);
+    }
+}
