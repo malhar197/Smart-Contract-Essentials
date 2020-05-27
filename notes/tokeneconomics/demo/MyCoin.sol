2766fac307fc9d4570ac8d6405d6dcd1e0a899b7
dhruvinparikh93 <imsilent93@gmail.com>
2020-05-27 15:51:38 -0400

    solve warning in MyCoin

--- notes/tokeneconomics/demo/MyCoin.sol
+++ notes/tokeneconomics/demo/MyCoin.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.6.0;
 
 library ConvertLib{
-	function convert(uint amount,uint conversionRate) returns (uint convertedAmount)
+	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
 	{
 		return amount * conversionRate;
 	}
